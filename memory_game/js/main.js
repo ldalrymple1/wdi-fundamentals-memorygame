89b249c716e83214e0be4e0aeb0bc73f16e64443
Lydia Dalrymple <dalrymple.lydia@gmail.com>
2019-08-05 15:33:52 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,27 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-var cardsInPlay = [];
-cardsInPlay.push (cardOne);
-cardsInPlay.push (cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[0]) {
-alert("You found a match!");
+var cards = ["queen", "queen", "king", "king"];
+cards.length;
+
+var cardsInPlay = [ ];
+
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[2]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[0]);
+
+  if (cards.length === 2) {
+    checkForMatch();
   }
-else {
-  alert("Sorry, try again.")
 }
-}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
